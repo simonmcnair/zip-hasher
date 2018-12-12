c46068f6111ff193e2056d4e15b1f925599bc193
The Promised Prince <debapriya.das.blr@gmail.com>
2018-12-12 11:10:35 +0530

    added tests.py

---
+++ tests.py
@@ -0,0 +1,15 @@
+import XMLSerialize
+
+data = {
+    'hey' : "123asd",
+    'there' : "he3jahdad",
+    'what' : [1, 2, 3, 4],
+    'is' : {
+        'name' : "Deba"
+    } 
+}
+
+a = XMLSerialize.Py2XML()
+a.parse(data)
+
+print a
