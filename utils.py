4ffacf6504d20324d26466d8cc241b8abe3e08bf
Azhor Ahai <debapriya.das.blr@gmail.com>
2018-12-12 08:26:57 +0530

    added md5 hash function

---
+++ utils.py
@@ -0,0 +1,38 @@
+import zipfile
+import os
+import hashlib
+
+def extractor(path_to_zip_file, directory_to_extract):
+    # extract zip files
+    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+        zip_ref.extractall(directory_to_extract_to)
+
+
+
+
+def getListOfFiles(dirName):
+    # create a list of file and sub directories 
+    # names in the given directory 
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory 
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+    
+    return allFiles
+
+
+def md5sum(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    
+    return hash_md5.hexdigest()
+
