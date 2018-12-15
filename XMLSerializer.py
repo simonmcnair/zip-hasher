f80d1321e20b4c31542e24bf6f7f9c6ab69e2ec1
The Promised Prince <debapriya.das.blr@gmail.com>
2018-12-15 21:04:26 +0530

    fix XML Serialization according to the format

--- XMLSerializer.py
+++ XMLSerializer.py
@@ -1,46 +1,23 @@
-import lxml
 import utils
+import collections
 
 from lxml import etree as ET
-from lxml import objectify
-import collections
-# tree = ET.parse(utils.get_xml_file())  
-# root = tree.getroot()
 
-# # one specific item attribute
-# print('Item #2 attribute:')  
-# print(root)
-
-
-# # all item attributes
-# print('\nAll attributes:')  
-# for elem in root:
-#     print elem
-#     print elem.attrib
-#     for subelem in elem:
-#         print(subelem.attrib)
-
-
-# # all items data
-# print('\nAll item data:')  
-# for elem in root:  
-#     for subelem in elem:
-#         print(subelem.text)
 
 def XMLSerialize(listOfData):
     rootDict = collections.OrderedDict(
         {
-            ts: '1493453326772'),
-            uri: "http://fxldemo.tornado.no/",
-            launch: "no.tornado.FxlDemo"
+            'ts' : "1493453326772",
+            'uri': "http://fxldemo.tornado.no/",
+            'launch': "no.tornado.FxlDemo"
         }
     )
     root = ET.Element('Application', collections.OrderedDict())
     for data in listOfData:
-        root.append(ET.Element('lib', data.getObjDetails))
+        root.append(ET.Element('lib', data.getObjDetails()))
     
     tree = ET.ElementTree(root)
-    tree.write('output.xml', pretty_print=True, xml_declaration=True, encoding="utf-8", standalone="yes")
+    tree.write(utils.get_xml_file(), pretty_print=True, xml_declaration=True, encoding="utf-8", standalone="yes")
 
 
 
