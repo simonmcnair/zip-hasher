2ef1010ca40fccd6f5da965ebb6ed78cc3b9d77b
101189766+simonmcnair@users.noreply.github.com <101189766+simonmcnair@users.noreply.github.com>
2024-01-08 15:05:56 +0000

    update

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import csv
 import sys
 from PIL import Image
 import utils
+from pathlib import Path
+
 
 exts = Image.registered_extensions()
 supported_image_extensions = {ex for ex, f in exts.items() if f in Image.OPEN}
@@ -16,6 +18,16 @@ supported_audio_extensions = ['.mp3','.flac']
 pritest = tempfile.gettempdir() # prints the current temporary directory
 createtempdir = tempfile.TemporaryDirectory()
 #f = tempfile.TemporaryFile()
+
+def get_script_name():
+    # Use os.path.basename to get the base name (script name) from the full path
+    #basename = os.path.basename(path)
+    return Path(__file__).stem
+    #return os.path.basename(__file__)
+
+def get_script_path():
+    return os.path.dirname(os.path.realpath(__file__))
+
 
 def main():
     """Main subroutine"""
@@ -31,11 +43,11 @@ def main():
     utils.logging.info('supported image extensions   : ' + str(supported_image_extensions))
     utils.logging.info('supported Audio extensions   : ' + str(supported_audio_extensions))
 
-    if os.path.isfile(csv_file_path):
+    if os.path.isfile(cache_file_path):
         utils.logging.info("csv file present")
         filename_array = []
 
-        with open(csv_file_path, 'r', encoding='utf-8', newline='') as csv_file:
+        with open(cache_file_path, 'r', encoding='utf-8', newline='') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             try:
                 for row in csv_reader:
@@ -47,101 +59,92 @@ def main():
         array = True
     else:
         utils.logging.info("No csv file, create one.")
-        with open(csv_file_path, mode='w', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file,quoting=csv.QUOTE_ALL)
-            header = ["filename","archive","type", "path to file if archive","hash"]
-            writer.writerow(header) 
-            array = False   
+        utils.writecsvrow(cache_file_path,["filename","archive","type", "path to file if archive","hash"])
 
-    with open(csv_file_path, mode='a', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file,quoting=csv.QUOTE_ALL)
+       # with open(cache_file_path, mode='w', newline='', encoding='utf-8') as file:
+       #     writer = csv.writer(file,quoting=csv.QUOTE_ALL)
+       #     header = ["filename","archive","type", "path to file if archive","hash"]
+       #     writer.writerow(header) 
+        array = False
 
-        if os.path.exists(dirtoprocess):
 
-            for root, dirs, files in os.walk(dirtoprocess):
+    if os.path.exists(dirtoprocess):
 
-                for file_name in files:
+        for root, dirs, files in os.walk(dirtoprocess):
 
-                    extension = os.path.splitext(file_name)[1].lower()
-                    full_file_path = os.path.join(root, file_name)
+            for file_name in files:
 
-                    if array is True and full_file_path in filename_array:
-                        utils.logging.info(f"file {full_file_path} is already in CSV.  Skipping and removing from array.  array size is " + str(len(filename_array)))
-                        # Remove filename from the array
-                        filename_array.remove(full_file_path)
+                extension = os.path.splitext(file_name)[1].lower()
+                full_file_path = os.path.join(root, file_name)
+
+                if array is True and full_file_path in filename_array:
+                    utils.logging.info(f"file {full_file_path} is already in CSV.  Skipping and removing from array.  array size is " + str(len(filename_array)))
+                    # Remove filename from the array
+                    filename_array.remove(full_file_path)
+                    continue
+                else:
+                    # Process the file (replace this with your processing logic)
+                    utils.logging.info(f"File not in CSV.  Processing file: {full_file_path}")
+
+                    if os.path.normpath(full_file_path) == os.path.normpath(cache_file_path) or os.path.normpath(full_file_path) == os.path.normpath(log_file_path):
+                        utils.logging.info("not processing csv or log file" + full_file_path)
                         continue
+
+                    isarchive = 'False'
+                    relativefilename = '-'
+
+                    if extension in supported_archive_extensions:
+                        utils.logging.info("Processing archive : " + full_file_path)
+                        isarchive = 'True'
+                        with tempfile.TemporaryDirectory() as path_to_extract:
+                            utils.logging.info('created temporary directory' + path_to_extract)
+                            result = utils.extractor(full_file_path, path_to_extract)
+                            if result is not False:
+                                list_of_all_files = utils.getListOfFiles(path_to_extract)
+                                for path_to_file in list_of_all_files:
+                                    utils.logging.info("processing " + path_to_file)
+                                    file_extension = os.path.splitext(path_to_file)[1].lower()
+                                    relativefilename = utils.stripfilepath(path_to_file)
+
+                                    if file_extension in supported_image_extensions:
+                                        hashret = utils.createimagehash(path_to_file)
+                                        filetype = 'image'
+                                    elif file_extension in supported_audio_extensions:
+                                        hashret = utils.createaudiohash(path_to_file)
+                                        filetype= 'audio'
+                                    else:
+                                        utils.logging.info("unknown file just hash it. " + file_extension + " " + path_to_file)
+                                        hashret = utils.calculate_blake2b(path_to_file)
+                                        filetype = 'other'
+
+                    elif extension in supported_image_extensions:
+                        utils.logging.info("Processing image : " + full_file_path)
+                        hashret = utils.createimagehash(full_file_path)
+                        filetype = 'image'
+
+                    elif extension in supported_audio_extensions:
+                        utils.logging.info("Processing audio : " + full_file_path)
+                        hashret = utils.createaudiohash(full_file_path)
+                        filetype = 'audio'
                     else:
-                        # Process the file (replace this with your processing logic)
-                        utils.logging.info(f"File not in CSV.  Processing file: {full_file_path}")
+                        utils.logging.info("Processing file : " + full_file_path)
+                        hashret = utils.calculate_blake2b(full_file_path)
+                        filetype = 'other'
 
-                        if os.path.normpath(full_file_path) == os.path.normpath(csv_file_path) or os.path.normpath(full_file_path) == os.path.normpath(log_file_path):
-                            utils.logging.info("not processing csv or log file" + full_file_path)
-                            continue
+                    if hashret is not False:
+                        utils.writecsvrow(cache_file_path,[full_file_path,isarchive,filetype,relativefilename,hashret])
+                    else:
+                        utils.logging.warning("no hash created for " + full_file_path)
 
-                        if extension in supported_archive_extensions:
-                            utils.logging.info("Processing archive : " + full_file_path)
-                            with tempfile.TemporaryDirectory() as path_to_extract:
-                                utils.logging.info('created temporary directory' + path_to_extract)
-                                result = utils.extractor(full_file_path, path_to_extract)
-                                if result is not False:
-                                    list_of_all_files = utils.getListOfFiles(path_to_extract)
-                                    for path_to_file in list_of_all_files:
-                                        utils.logging.info("processing " + path_to_file)
-                                        file_extension = os.path.splitext(path_to_file)[1].lower()
-                                        relativefilename = utils.stripfilepath(path_to_file)
-
-                                        if file_extension in supported_image_extensions:
-                                            hashret = utils.createimagehash(path_to_file)
-                                            filetype = 'image'
-                                        elif file_extension in supported_audio_extensions:
-                                            hashret = utils.createaudiohash(path_to_file)
-                                            filetype= 'audio'
-                                        else:
-                                            utils.logging.info("unknown file just hash it. " + file_extension + " " + path_to_file)
-                                            hashret = utils.calculate_blake2b(path_to_file)
-                                            filetype = 'other'
-
-                                        if hashret is not False:
-                                            #utils.writecsvrow(csv_file_path,[full_file_path,'archive',relativefilename,hash])
-                                            writer.writerow([full_file_path,'archive',filetype,relativefilename,hashret])
-                                        else:
-                                            utils.logging.warning("no hash created for " + full_file_path)
-
-
-                        elif extension in supported_image_extensions:
-                            utils.logging.info("Processing image : " + full_file_path)
-                            hashret = utils.createimagehash(full_file_path)
-                            if hashret is not False:
-                                #utils.writecsvrow(csv_file_path,[full_file_path,'image','-',hash])
-                                writer.writerow([full_file_path,'-','image','-',hashret])
-                            else:
-                                utils.logging.warning("no hash created for " + full_file_path)
-
-                        elif extension in supported_audio_extensions:
-                            utils.logging.info("Processing audio : " + full_file_path)
-                            hashret = utils.createaudiohash(full_file_path)
-                            if hashret is not False:
-                                #utils.writecsvrow(csv_file_path,[full_file_path,'audio','-',hash])
-                                writer.writerow([full_file_path,'-','audio','-',hashret])
-                            else:
-                                utils.logging.warning("no hash created for " + full_file_path)
-                        else:
-                            utils.logging.info("Processing file : " + full_file_path)
-                            hashret = utils.calculate_blake2b(full_file_path)
-                            if hashret is not False:
-                                #utils.writecsvrow(csv_file_path,[full_file_path,'other','-',hash])
-                                writer.writerow([full_file_path,'-','other','-',hashret])
-                            else:
-                                utils.logging.warning("no hash created for " + full_file_path)
 
     with open(remainfile, 'w', encoding='utf-8', newline='') as log_file:
         for remaining_file in filename_array:
             log_file.write(f"Unprocessed file: {remaining_file}\n")
 
 
-    utils.remove_duplicates(csv_file_path,dupefilepath,'filename')
+    utils.remove_duplicates(cache_file_path,dupefilepath,'filename')
     utils.remove_unique_hashes(dupefilepath,sortedfilepath,'hash')
-    utils.sortcsv(sortedfilepath,sortedfilepath,'hash')
+    result = utils.sortcsv(sortedfilepath,sortedfilepath,'hash')
 
     if result is True :
         utils.logging.info("Sort success")
@@ -150,49 +153,46 @@ def main():
 
 
 #processing_dir = os.path.dirname(os.path.abspath(__file__))
+localoverridesfile = os.path.join(get_script_path(), "localoverridesfile_" + get_script_name() + '.py')
+log_file_path =  os.path.join(get_script_path(),get_script_name() + '.log')
+cache_file_path =  os.path.join(get_script_path(),get_script_name() + '.cache')
 
 if __name__=='__main__':
     if len(sys.argv) > 1:
 
         parser = argparse.ArgumentParser(description='Process some zip files to an XML.')
-        #parser.add_argument('-d', '--dir', action="store", dest="dir", type=str, help="pass the path to zip files", required=False,default="/srv/dev-disk-by-uuid-342ac512-ae09-47a7-842f-d3158537d395/mnt/Comics")
-        #parser.add_argument('-o', '--outputfile', action="store", dest="output", type=str, help="outputcsv", required=False,default="/srv/dev-disk-by-uuid-342ac512-ae09-47a7-842f-d3158537d395/mnt/Comics/output.csv")
-        #parser.add_argument('-l', '--logfile', action="store", dest="logfile", type=str, help="log file", required=False,default="/srv/dev-disk-by-uuid-342ac512-ae09-47a7-842f-d3158537d395/mnt/Comics/logfile.txt")
 
-        parser.add_argument('-d', '--dir', action="store", dest="dir", type=str, help="pass the path to zip files", required=False,default="/srv/dev-disk-by-uuid-342ac512-ae09-47a7-842f-d3158537d395/mnt/Audio")
-        parser.add_argument('-o', '--outputfile', action="store", dest="outputcsv", type=str, help="path to csv file", required=False,default="/srv/dev-disk-by-uuid-342ac512-ae09-47a7-842f-d3158537d395/mnt/Audio/output.csv")
-        parser.add_argument('-l', '--logfile', action="store", dest="log_file_path", type=str, help="log file path", required=False,default="/srv/dev-disk-by-uuid-342ac512-ae09-47a7-842f-d3158537d395/mnt/Audio/logfile.txt")
-        parser.add_argument('-r', '--remainder', action="store", dest="remainfile", type=str, help="remain file path", required=False,default="/srv/dev-disk-by-uuid-342ac512-ae09-47a7-842f-d3158537d395/mnt/Audio/remain.txt")
-
-        #parser.add_argument('-d', '--dir', action="store", dest="dir", type=str, help="pass the path to zip files", required=False,default="Z:/Audio")
-        #parser.add_argument('-o', '--outputfile', action="store", dest="outputcsv", type=str, help="outputcsv", required=False,default="Z:/Audio/output.csv")
-        #parser.add_argument('-l', '--logfile', action="store", dest="logfile", type=str, help="log file", required=False,default="Z:/Audio/logfile.txt")
-        #parser.add_argument('-r', '--remainder', action="store", dest="remainfile", type=str, help="remain file path", required=False,default="z:/Audio/remain.txt")
+        parser.add_argument('-d', '--dir', action="store", dest="dir", type=str, help="pass the path to zip files", required=False,default="/folder/to/index")
+        parser.add_argument('-o', '--outputfile', action="store", dest="outputcsv", type=str, help="path to csv file", required=False,default="/folder/to/output.csv")
+        #parser.add_argument('-l', '--logfile', action="store", dest="log_file_path", type=str, help="log file path", required=False,default="/folder/to/logfile.txt")
+        parser.add_argument('-r', '--remainder', action="store", dest="remainfile", type=str, help="remain file path", required=False,default="/folder/to/remain.txt")
 
         args = parser.parse_args()
         dirtoprocess = os.path.normpath(args.dir)
         csv_file_path = os.path.normpath(args.outputcsv)
-        log_file_path = os.path.normpath(args.logfile)
+        #log_file_path = os.path.normpath(args.logfile)
         remainfile = os.path.normpath(args.remainfile)
-
-        #processing_dir = utils.get_directory(csv_file_path)
-
-        sortedfilepath = os.path.join(dirtoprocess, 'sorted.csv')
-        dupefilepath = os.path.join(dirtoprocess, 'dupepath.csv')
+        cache_file_path = os.path.join(get_script_path(),   'cache.csv')
+        sortedfilepath = os.path.join(get_script_path(),  'sorted.csv')
+        dupefilepath = os.path.join(get_script_path(),  'dupepath.csv')
 
         utils.setup_logging(log_file_path)
         main()
     else:
-        #processing_dir = utils.get_directory(csv_file_path)
 
-        #dirtoprocess = "/srv/dev-disk-by-uuid-342ac512-ae09-47a7-842f-d3158537d395/mnt/Audio"
-        dirtoprocess = "Z:/Audio"
-        csv_file_path = os.path.join(dirtoprocess, 'output.csv')
-        log_file_path = os.path.join(dirtoprocess, 'logfile.log')
-        remainfile = os.path.join(dirtoprocess, 'remainfile.log')
+        dirtoprocess = "/folder/to/index"
+        csv_file_path = os.path.join(get_script_path(),   'output.csv')
+        cache_file_path = os.path.join(get_script_path(),   'cache.csv')
+        remainfile = os.path.join(get_script_path(),   'remainfile.log')
+        sortedfilepath = os.path.join(get_script_path(),  'sorted.csv')
+        dupefilepath = os.path.join(get_script_path(),  'dupepath.csv')
 
-        sortedfilepath = os.path.join(dirtoprocess, 'sorted.csv')
-        dupefilepath = os.path.join(dirtoprocess, 'dupepath.csv')
+        if os.path.exists(localoverridesfile):
+            exec(open(localoverridesfile).read())
+            #api_key = apikey
+            #print("API Key:", api_key)
+        else:
+            print("No local overrides.")
 
         utils.setup_logging(log_file_path)
         main()
